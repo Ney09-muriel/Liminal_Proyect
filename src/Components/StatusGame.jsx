bd4f59a56b9d9bbf6dbc6431559ca10ffd74de98
afsaanchez04 <afsanchez04@gmail.com>
2026-04-30 11:18:58 -0500

    vidas status

--- src/Components/StatusGame.jsx
+++ src/Components/StatusGame.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 
 export const StatusGame = () => {
   const [gemas, setGemas] = useState("");
+  const [vidas, setVidas] = useState("")
 return (
     
     <div>StatusGame</div>
