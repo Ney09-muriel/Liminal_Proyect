13bb0efea3fc18a5c17dea36a3fc3ca92cf76198
A1exis0o7 <shindatensei24@gmail.com>
2026-05-04 10:46:27 -0500

    titulo statusgame

--- src/Components/StatusGame.jsx
+++ src/Components/StatusGame.jsx
@@ -6,12 +6,14 @@ export const StatusGame = () => {
 return (
     <>
     <div className="vidas">
+      <h1>Vidas: ____</h1>
       <button>Vida 1</button>
       <button>Vida 2</button>
       <button>Vida 3</button>
     </div>
 
     <div className="gemas">
+      <h1>Gemas: ____</h1>
       <button>Gema 1</button>
       <button>Gema 2</button>
       <button>Gema 3</button>
