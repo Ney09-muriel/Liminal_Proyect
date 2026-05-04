967b962a7ccf054ba8e693cd751fe678f4c1da99
Ney09-muriel <shindatensei24@gmail.com>
2026-05-04 10:50:18 -0500

    nombre

--- src/Components/StatusGame.jsx
+++ src/Components/StatusGame.jsx
@@ -6,14 +6,14 @@ export const StatusGame = () => {
 return (
     <>
     <div className="vidas">
-      <h1>Vidas: ____</h1>
+      <h1>Vidas: _____</h1>
       <button>Vida 1</button>
       <button>Vida 2</button>
       <button>Vida 3</button>
     </div>
 
     <div className="gemas">
-      <h1>Gemas: ____</h1>
+      <h1>Gemas: _____</h1>
       <button>Gema 1</button>
       <button>Gema 2</button>
       <button>Gema 3</button>
