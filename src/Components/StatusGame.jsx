f50cff178fdbbfb6f6886017de13c01505b7580e
Ney09-muriel <shindatensei24@gmail.com>
2026-05-04 11:22:22 -0500

    lista la sumatoria

--- src/Components/StatusGame.jsx
+++ src/Components/StatusGame.jsx
@@ -6,17 +6,17 @@ export const StatusGame = () => {
 return (
     <>
     <div className="vidas">
-      <h1>❤️ Vidas: _____</h1>
-      <button onClick={() => setVidas(prev => prev + 1)}>Vida 1</button>
+      <h1>❤️ Vidas: {vidas} </h1>
+      <button onClick={() => setVidas(prev => prev + 5)}>Vida 1</button>
       <button onClick={() => setVidas(prev => prev + 1)}>Vida 2</button>
-      <button onClick={() => setVidas(prev => prev + 1)}>Vida 3</button>
+      <button onClick={() => setVidas(prev => prev + 2)}>Vida 3</button>
     </div>
 
     <div className="gemas">
-      <h1>💎 Gemas: _____</h1>
-      <button onClick={() => setGemas(prev => prev + 1)}>Gemas del infinito</button>
-      <button onClick={() => setGemas(prev => prev + 1)}>Gemas oscuras</button>
-      <button onClick={() => setGemas(prev => prev + 1)}>Gemas de la luz</button>
+      <h1>💎 Gemas: {gemas} </h1>
+      <button onClick={() => setGemas(prev => prev + 4)}>Gemas del infinito</button>
+      <button onClick={() => setGemas(prev => prev + 2)}>Gemas oscuras</button>
+      <button onClick={() => setGemas(prev => prev + 5)}>Gemas de la luz</button>
     </div>
     </>
   )
