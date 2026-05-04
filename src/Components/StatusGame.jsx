3396edb419ab3aa8746ea50bebd4bb497dea0854
Ney09-muriel <shindatensei24@gmail.com>
2026-05-04 11:04:55 -0500

    botones

--- src/Components/StatusGame.jsx
+++ src/Components/StatusGame.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
 export const StatusGame = () => {
-  const [gemas, setGemas] = useState("");
-  const [vidas, setVidas] = useState("")
+  const [gemas, setGemas] = useState(0);
+  const [vidas, setVidas] = useState(0)
 return (
     <>
     <div className="vidas">
       <h1>Vidas: _____</h1>
-      <button>Vida 1</button>
-      <button>Vida 2</button>
-      <button>Vida 3</button>
+      <button onClick={() => setVidas(prev => prev + 1)}>Vida 1</button>
+      <button onClick={() => setVidas(prev => prev + 1)}>Vida 2</button>
+      <button onClick={() => setVidas(prev => prev + 1)}>Vida 3</button>
     </div>
 
     <div className="gemas">
-      <h1>Gemas: _____</h1>
-      <button>Gema 1</button>
-      <button>Gema 2</button>
-      <button>Gema 3</button>
+      <h1>💎 Gemas: _____</h1>
+      <button onClick={() => setGemas(prev => prev + 1)}>Gemas del infinito</button>
+      <button onClick={() => setGemas(prev => prev + 1)}>Gemas oscuras</button>
+      <button onClick={() => setGemas(prev => prev + 1)}>Gemas de la luz</button>
     </div>
     </>
   )
